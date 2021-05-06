a7b0503e9640320769a10f89a7e99ebd8e7bd647
Stanislav Bakryu <stasbakryu@gmail.com>
2021-05-06 20:27:16 +0300

    add Device Controller func

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,7 +1,68 @@
-class DeviceController {
-  async create(req, res) {}
+const uuid = require("uuid");
+const path = require("path");
+const { Device } = require("../models/models");
+const ApiError = require("../errors/ApiError");
 
-  async getAll(req, res) {}
+class DeviceController {
+  async create(req, res, next) {
+    try {
+      const { name, price, brandId, typeId, info } = req.body;
+      const { img } = req.files;
+      let fileName = uuid.v4() + ".jpg"; //unique name generator
+      img.mv(path.resolve(__dirname, "..", "static", fileName)); //create path to img
+
+      const device = await Device.create({
+        name,
+        price,
+        brandId,
+        typeId,
+        info,
+        img: fileName,
+      });
+
+      return res.json(device);
+    } catch (error) {
+      ApiError.badRequest(error.message);
+    }
+  }
+
+  async getAll(req, res) {
+    let { brandId, typeId, limit, page } = req.query;
+    page = page || 1;
+    limit = limit || 9;
+    let offset = page * limit - limit;
+    let devices;
+
+    if (!brandId && !typeId) {
+      devices = await Device.findAndCountAll({ limit, offset });
+    }
+
+    if (brandId && !typeId) {
+      devices = await Device.findAndCountAll({
+        where: { brandId },
+        limit,
+        offset,
+      });
+    }
+
+    if (!brandId && typeId) {
+      devices = await Device.findAndCountAll({
+        where: { typeId },
+        limit,
+        offset,
+      });
+    }
+
+    if (brandId && typeId) {
+      devices = await Device.findAndCountAll({
+        where: { brandId, typeId },
+        limit,
+        offset,
+      });
+    }
+
+    return res.json(devices);
+  }
 
   async getOne(req, res) {}
 }
